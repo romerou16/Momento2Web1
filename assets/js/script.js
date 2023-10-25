7a422cc4cdd0dc2c76b5f448523df99eb91755b6
Daniela Romero <dsromero2911@gmail.com>
2023-10-25 11:27:07 -0500

    0.1

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function abrirVentana_a(){
+    let url = "/assets/sub/mexico.html";
+    if (url) {
+        window.open(url,"_blank","width=600,height=480");
+    }
+}
+function abrirVentana_b(){
+    let url = "/assets/sub/brasil.html";
+    if (url){
+        window.open(url,"_blank","width=600,height=480");
+    }
+}
+function abrirVentana_c(){
+    let url = "/assets/sub/amsterdam.html";
+    if (url){
+        window.open(url,"_blank","width=600,height=480");
+    }
+}
